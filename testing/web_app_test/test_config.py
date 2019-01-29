e9fc80c8f3fad2f74a638ac13b92d5a7c114b118
llyntech <42455013+llyntech@users.noreply.github.com>
2019-01-29 14:54:46 -0800

    Update test_config.py

--- testing/web_app_test/test_config.py
+++ testing/web_app_test/test_config.py
@@ -1,6 +1,7 @@
 """
 This module is holding all the test configuration data.
 All customized the data, such as uri, reference image, etc. should be defined here.
+Typing something here
 """
 hub_url="http://127.0.0.1:4444/wd/hub"
 app_url="https://docs.pytest.org/en/latest/"
